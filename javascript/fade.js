b70fee681c083547925b077a1161c6b015a6a65b
Wardium <84645455+Wardium@users.noreply.github.com>
2026-04-02 00:52:28 -0700

    Update fade.js

--- javascript/fade.js
+++ javascript/fade.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", () => {
   const overlay = document.getElementById("bg-fade-overlay");
-  
-  // Add the class that triggers the CSS transition
-  overlay.classList.add("fade-out");
 
   // Optional: Remove it from the DOM entirely after the transition
   overlay.addEventListener("transitionend", () => overlay.remove());
