c49b9412ba3dbe6218503f5f6dc29845d14ef5f9
Wardium <84645455+Wardium@users.noreply.github.com>
2026-04-07 15:36:04 -0700

    Create title.js

---
+++ javascript/title.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Wait 2 seconds, then clean up the title for the user
+    setTimeout(() => {
+        document.title = "Dylan Ward Studios";
+    }, 2000); 
+});
